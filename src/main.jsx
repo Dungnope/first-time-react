515bae0d0211f4c5c3a1850d0ce716fc49c22ed4
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-02 22:21:48 +0700

    change import name of default export FoodGallery

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import FoodGallery from './foods.jsx'
+import FoodMenu from './foods.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FoodGallery />
+    <FoodMenu />
   </StrictMode>,
 )
