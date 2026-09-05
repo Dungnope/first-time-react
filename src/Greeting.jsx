4aebc3356595240ea5769b3d6d20b94c4fb254c3
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-05 14:59:18 +0700

    render a list of elements JSX

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,17 @@
-function Hello() {
+export function Hello() {
     return <h1>"I swear by my pretty floral bonnet, I will end you."</h1>
 }
 
-export default Hello;
+export function ListOfElement() {
+    const animals = ["lions", "cow", "snake", "lizard"];
+    return (
+        <>
+            <h1>Animals: </h1>
+            <ul>
+                {animals.map(animal => {
+                    return <li key={animal}>{animal}</li>
+                })}
+            </ul>
+        </>
+    )
+}
